848f37626610dfb9df3fe1d0db5f7d8f744979e7
lidustus-ops <lidustus@gmail.com>
2026-02-26 13:09:48 +0200

    Parallel solve

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
+#include <thread> 
 #include <chrono>
 #include <cstdlib> 
 #include <ctime>   
@@ -20,26 +21,44 @@ void solveSequential(int N, vector<vector<int>>& matrix) {
     for (int j = 0; j < N; ++j) {
         int max_val = matrix[0][j];
         for (int i = 1; i < N; ++i) {
-            if (matrix[i][j] > max_val) {
-                max_val = matrix[i][j];
-            }
+            if (matrix[i][j] > max_val) max_val = matrix[i][j];
         }
         matrix[j][j] = max_val;
     }
 }
 
+void partialSolve(int N, vector<vector<int>>* matrix, int start_col, int end_col) {
+    for (int j = start_col; j < end_col; ++j) {
+        int max_val = (*matrix)[0][j];
+        for (int i = 1; i < N; ++i) {
+            if ((*matrix)[i][j] > max_val) max_val = (*matrix)[i][j];
+        }
+        (*matrix)[j][j] = max_val;
+    }
+}
+
+double solveParallel(int N, vector<vector<int>>& matrix, int num_threads) {
+    vector<thread> threads;
+    int chunk = N / num_threads;
+    auto start = high_resolution_clock::now();
+
+    for (int i = 0; i < num_threads; ++i) {
+        int start_col = i * chunk;
+        int end_col = (i == num_threads - 1) ? N : (i + 1) * chunk;
+        threads.emplace_back(partialSolve, N, &matrix, start_col, end_col);
+    }
+    for (auto& th : threads) th.join();
+
+    auto end = high_resolution_clock::now();
+    return duration<double>(end - start).count();
+}
+
 int main() {
     srand(static_cast<unsigned int>(time(0)));
-    int N = 2000;
+    int N = 5000;
     vector<vector<int>> matrix(N, vector<int>(N));
     fillMatrix(matrix, N);
 
-    auto start = high_resolution_clock::now();
-    solveSequential(N, matrix);
-    auto end = high_resolution_clock::now();
-
-    duration<double> elapsed = end - start;
-    cout << "Sequential time for 2000x2000: " << elapsed.count() << "s" << endl;
-
+    cout << "Parallel time (4 threads): " << solveParallel(N, matrix, 4) << "s" << endl;
     return 0;
 }
